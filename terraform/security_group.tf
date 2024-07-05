10cdb067bbc913b1470331c5eee1429adbee4ded
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-05 11:41:28 +0530

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,9 +1,30 @@
+# security_groups.tf
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 1337
     to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "lb" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
