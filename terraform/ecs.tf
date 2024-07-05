3a761273b112e5dd810e40a1f682c1419edfdec8
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-05 11:42:16 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,4 @@
+# ecs.tf
 resource "aws_ecs_cluster" "main" {
   name = "strapi-cluster"
 }
@@ -12,7 +13,7 @@ resource "aws_ecs_task_definition" "strapi" {
   container_definitions = jsonencode([
     {
       name      = "strapi"
-      image     = "veera1016/strapi:latest"
+      image     = "your-dockerhub-username/your-strapi-image:latest"
       essential = true
       portMappings = [
         {
@@ -22,4 +23,24 @@ resource "aws_ecs_task_definition" "strapi" {
       ]
     }
   ])
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    security_groups  = [aws_security_group.main.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
 }
