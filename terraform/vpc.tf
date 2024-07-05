2011101ae176416a63123ad58230a82e77916dad
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-05 11:52:17 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,6 +7,7 @@ resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
 }
 
@@ -28,3 +29,5 @@ resource "aws_route_table_association" "a" {
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public.id
 }
+
+data "aws_availability_zones" "available" {}
