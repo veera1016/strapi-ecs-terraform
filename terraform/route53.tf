9576e57cb5b1f571e644d9efc8eb0eb2a61c5563
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-05 11:25:33 +0530

    Create route53.tf

---
+++ terraform/route53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "strapi" {
+  zone_id = "Z06607023RJWXGXD2ZL6M" # Replace with your Route 53 hosted zone ID
+  name    = "togaruashok1996.contentecho.in" # Replace with your subdomain
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = false
+  }
+}
