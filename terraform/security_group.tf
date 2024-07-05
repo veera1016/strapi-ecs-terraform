308073f2de428dad69663c1ab40a77d0ad376601
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-05 11:21:52 +0530

    Create security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
